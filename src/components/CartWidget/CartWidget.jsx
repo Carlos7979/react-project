a1e183d76eb76d79e400ecc957e5d00622a90b9b
Carlos7979 <carloscorredorwg@gmail.com>
2023-11-26 16:25:35 -0400

    add bubble to CartWidget component

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,9 +2,13 @@ import cartShopping from '../../assets/cart-shopping.svg'
 import styles from './CartWidget.module.css'
 
 const CartWidget = () => {
+    const notificationValue = 15
     return (
         <>
-			<img className={styles.cartShopping} src={cartShopping} alt="" />
+            <img className={styles.cartShopping} src={cartShopping} alt='' />
+            <div className={styles.notificationBubbleContainer}>
+				<div className={styles.notificationBubble}>{notificationValue}</div>
+			</div>
         </>
     )
 }
